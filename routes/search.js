ba31eb767ab2893f3bd79bf31b5de55ca6a31400
LeeSeungYun1020 <ileilliat@gmail.com>
2020-12-26 23:08:48 +0900

    Complete case search.

--- routes/search.js
+++ routes/search.js
@@ -2,25 +2,51 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../lib/mysql')
 
-router.get('/type', function(req, res, next) {
-  res.render('type.ejs')
+router.get('/type', function (req, res, next) {
+    res.render('type.ejs')
 })
 
-router.post('/type/:type/:attr', function(req, res, next) {
-  mysql.query(`SELECT * from ${req.params.type} where ${req.params.attr} LIKE ?`,
-      ["%" + req.body.text + "%"],
-      function (error, results) {
-        console.log(error)
-        return res.send({error: error, body: results})
-      })
+router.post('/type/:type/:attr', function (req, res, next) {
+    mysql.query(`SELECT * from ${req.params.type} where ${req.params.attr} LIKE ?`,
+        ["%" + req.body.text + "%"],
+        function (error, results) {
+            console.log(error)
+            return res.send({error: error, body: results})
+        })
 })
 
 router.get('/case', function (req, res, next) {
-  res.render('case.ejs')
+    res.render('case.ejs')
 })
 
 router.post('/case/1', function (req, res, next) {
-  res.send({error: error, body: results})
+    mysql.query(`
+select product.name
+from (select trade.productID, count(trade.transactionnumber) as count
+      from customer,
+           trade
+      where name = customerName
+        and customer.gender = 'Female'
+      group by trade.productID) female,
+     (select productID, 0 as count
+      from product
+      where productID not in (select trade.productID
+                              from customer, trade
+                              where name = customerName and customer.gender = 'Male')
+      UNION
+      select trade.productID, count(trade.transactionnumber) as count
+      from customer,
+           trade
+      where name = customerName
+        and customer.gender = 'Male'
+      group by trade.productID) male, product
+where (female.productID = male.productID and female.count > male.count and female.productID = product.productID)
+`.trim(),
+        [req.body.date],
+        function (error, results) {
+            console.log(error)
+            return res.send({error: error, body: results})
+        })
 })
 
 router.post('/case/2', function (req, res, next) {
@@ -54,11 +80,11 @@ router.post('/case/3', function (req, res, next) {
 
 // 라이브러리 파일 요청
 router.get('/*.ejs', (req, res) => {
-  res.render(req.params[0] + '.ejs')
+    res.render(req.params[0] + '.ejs')
 })
 
 router.get('/javascripts/*.js', (req, res) => {
-  res.redirect(`/javascripts/${req.params[0]}.js`)
+    res.redirect(`/javascripts/${req.params[0]}.js`)
 })
 
 module.exports = router;
